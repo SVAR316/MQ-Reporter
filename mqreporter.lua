d518a53c449e0f7c71ddbd6b3b92b2b6a1d1b7ef
Artem <74237346+ZloyMolodoy@users.noreply.github.com>
2023-07-28 01:53:05 +0400

    update 0.0.2

--- mqreporter.lua
+++ mqreporter.lua
@@ -1,9 +1,47 @@
 script_name("MQ Reporter")
 script_author("zloymolodoy")
-script_version("0.0.1")
+script_version("0.0.2")
 
-local sampev   = require 'lib.samp.events'
+local sampev = require 'lib.samp.events'
 local encoding = require 'encoding'
 
+--Кодировка
 encoding.default = 'cp1251'
 u8 = encoding.UTF8
+
+local active = false
+local sendMessage = true
+
+function main()
+    repeat wait(0) until isSampAvailable()
+    --..thisScript().script_version
+    sampAddChatMessage("{2078be}[MQ]{FFFFFF}Скрипт MQ reporter запущен. /mq активация скрипта. Версия:", -1)
+
+    sampRegisterChatCommand("mq", function()
+        if active == false then
+            sampAddChatMessage("{2078be}MQ Включен")
+            active = true
+        else
+            sampAddChatMessage("{2078be}MQ Выключен")
+            active = false
+        end
+    end)
+
+    function sendReportMessage()
+        sampSendChat("/report mq")
+    end
+
+    function sampev.onSendSpawn()
+        sendMessage = true
+    end
+
+    while true do wait(0)
+        if active == true and sendMessage == true then
+            if getCharHealth(PLAYER_PED) <= 0 then
+                sendMessage = false
+                sendReportMessage()
+            end
+        end
+    end
+
+end
